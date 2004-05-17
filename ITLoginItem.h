c40f5440e35326e867b0fa87ec9963d41205380c
Kent Sutherland <ksuther@ithinksw.com>
2004-05-17 22:36:16 +0000

    The ITLoginItem function(s) will easily set an app to autolaunch at login.

---
+++ ITLoginItem.h
@@ -0,0 +1,11 @@
+/*
+ *  ITLoginItem.h
+ *  ITFoundation
+ *
+ *  Created by Kent Sutherland on Sun May 16 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <Carbon/Carbon.h>
+#include <Foundation/Foundation.h>
---
+++ ITLoginItem.m
@@ -0,0 +1,52 @@
+/*
+ *  ITLoginItem.c
+ *  ITFoundation
+ *
+ *  Created by Kent Sutherland on Sun May 16 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "ITLoginItem.h"
+#import "ITDebug.h"
+
+BOOL ITSetLaunchApplicationOnLogin(NSString *path, BOOL flag)
+{
+    NSUserDefaults *df = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *loginwindow;
+    NSMutableArray *loginarray;
+    FSRef fileRef;
+    AliasHandle alias;
+    NSData *aliasData;
+    
+    ITDebugLog(@"Set if MenuPrefs launches at login to %i.", flag);
+    [df synchronize];
+    loginwindow = [[df persistentDomainForName:@"loginwindow"] mutableCopy];
+    loginarray = [loginwindow objectForKey:@"AutoLaunchedApplicationDictionary"];
+    
+    //Create the alias data
+    FSMakeRefWithPath([path UTF8String], &fileRef);
+    FSNewAlias(NULL, &fileRef, &alias);
+    aliasData = [NSData dataWithBytes:&alias length:sizeof(alias)];
+    
+    if (flag) {
+        NSDictionary *itemDict = [NSDictionary dictionaryWithObjectsAndKeys:
+        [[NSBundle mainBundle] bundlePath], @"Path",
+        [NSNumber numberWithInt:0], @"Hide",
+        aliasData, @"AliasData", nil, nil];
+        [loginarray addObject:itemDict];
+    } else {
+        int i;
+        for (i = 0; i < [loginarray count]; i++) {
+            NSDictionary *tempDict = [loginarray objectAtIndex:i];
+            if ([[[tempDict objectForKey:@"Path"] lastPathComponent] isEqualToString:[[[NSBundle mainBundle] bundlePath] lastPathComponent]]) {
+                [loginarray removeObjectAtIndex:i];
+                break;
+            }
+        }
+    }
+    [df setPersistentDomain:loginwindow forName:@"loginwindow"];
+    [df synchronize];
+    [loginwindow release];
+    return YES;
+}
