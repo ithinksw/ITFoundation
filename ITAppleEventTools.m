0471d3908d12b15bd1962ad57402da9f838ce66c
Kent Sutherland <ksuther@ithinksw.com>
2004-09-25 23:52:16 +0000

    Alex's memory leak fixes and timeout changes.

--- ITAppleEventTools.m
+++ ITAppleEventTools.m
@@ -39,7 +39,7 @@ NSAppleEventDescriptor *ITSendAEWithString(NSString *sendString, FourCharCode ev
         ITDebugLog(@"Error: %d:%d at \"%@\"",(int)buildError.fError,buildError.fErrorPos,[sendString substringToIndex:buildError.fErrorPos]);
     }
     
-    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/100, NULL, NULL);
+    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/60, NULL, NULL);
     
     err = AESizeOfParam(&replyEvent, keyDirectObject, &resultType, &resultSize);
     if (resultSize == 0 || err != 0) {
@@ -49,6 +49,7 @@ NSAppleEventDescriptor *ITSendAEWithString(NSString *sendString, FourCharCode ev
     AEGetParamDesc(&replyEvent, keyDirectObject, resultType, &resultDesc);
     
     recv = [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&resultDesc] autorelease];
+    [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&replyEvent] autorelease];
     if (!err) [recv logDesc];
     
     if (err) {
@@ -86,7 +87,7 @@ NSAppleEventDescriptor *ITSendAEWithStringAndObject(NSString *sendString, const 
         ITDebugLog(@"Error: %d:%d at \"%@\"",(int)buildError.fError,buildError.fErrorPos,[sendString substringToIndex:buildError.fErrorPos]);
     }
     err = AEPutParamDesc(&sendEvent, keyDirectObject, object);
-    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/100, NULL, NULL);
+    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/60, NULL, NULL);
     
     err = AESizeOfParam(&replyEvent, keyDirectObject, &resultType, &resultSize);
     if (resultSize == 0 || err != 0) {
@@ -96,6 +97,7 @@ NSAppleEventDescriptor *ITSendAEWithStringAndObject(NSString *sendString, const 
     AEGetParamDesc(&replyEvent, keyDirectObject, resultType, &resultDesc);
     
     recv = [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&resultDesc] autorelease];
+    [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&replyEvent] autorelease];
     if (!err) [recv logDesc];
     
     if (err) {
@@ -133,7 +135,7 @@ NSAppleEventDescriptor *ITSendAEWithKey(FourCharCode reqKey, FourCharCode evClas
         ITDebugLog(@"Error: %d:%d at \"%@\"",(int)buildError.fError,buildError.fErrorPos,[sendString substringToIndex:buildError.fErrorPos]);
     }
     
-    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/100, NULL, NULL);
+    err = AESend(&sendEvent, &replyEvent, kAEWaitReply, kAENormalPriority, /*kAEDefaultTimeout*/60, NULL, NULL);
     
     err = AESizeOfParam(&replyEvent, keyDirectObject, &resultType, &resultSize);
     if (resultSize == 0 || err != 0) {
@@ -143,6 +145,7 @@ NSAppleEventDescriptor *ITSendAEWithKey(FourCharCode reqKey, FourCharCode evClas
     AEGetParamDesc(&replyEvent, keyDirectObject, resultType, &resultDesc);
     
     recv = [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&resultDesc] autorelease];
+    [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&replyEvent] autorelease];
     if (!err) [recv logDesc];
     
     if (err) {
@@ -168,7 +171,7 @@ NSAppleEventDescriptor *ITSendAE(FourCharCode eClass, FourCharCode eID,const Pro
     cerr2 = AECreateAppleEvent(eClass,eID,&dest,kAutoGenerateReturnID,kAnyTransactionID,&event);
     nse = [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&event] autorelease];
     if (!cerr2) [nse logDesc];
-    err = AESend(&event, &reply, kAENoReply, kAENormalPriority, /*kAEDefaultTimeout*/100, NULL, NULL);
+    err = AESend(&event, &reply, kAENoReply, kAENormalPriority, /*kAEDefaultTimeout*/60, NULL, NULL);
     nsr = [[[NSAppleEventDescriptor alloc] initWithAEDescNoCopy:&reply] autorelease];
     [nsr logDesc];
     return nsr;
