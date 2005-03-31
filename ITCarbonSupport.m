034559d9a2f0b8e607b0365152b9c73732f00e6e
Joseph Spiros <joseph.spiros@ithinksw.com>
2005-03-31 04:33:42 +0000

    Fixing Alex's crazy tab hijinks.

--- ITCarbonSupport.m
+++ ITCarbonSupport.m
@@ -6,11 +6,11 @@ NSString *NSStringFromFourCharCode(unsigned long code) {
 
 unsigned long FourCharCodeFromNSString(NSString *string) {
 	const unsigned char *c_s = [string UTF8String];
-    unsigned long tmp = *c_s++;
-    tmp <<= 8;
-    tmp |= *c_s++;
-    tmp <<= 8;
-    tmp |= *c_s++;
-    tmp <<= 8;
-    return tmp |= *c_s++;
+	unsigned long tmp = *c_s++;
+	tmp <<= 8;
+	tmp |= *c_s++;
+	tmp <<= 8;
+	tmp |= *c_s++;
+	tmp <<= 8;
+	return tmp |= *c_s++;
 }
